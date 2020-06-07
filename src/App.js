89b8e695c6a6bdaee0d124cc36124291e35464ba
anggasayogo <anggasayogosm@gmail.com>
2020-06-07 07:03:12 +0700

    initial project

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Profile from './Profile';
 
 function App() {
   return (
---
+++ src/Profile.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Profile() {
+    return (
+        <div>
+            <p>Hello Ini profil app</p>
+        </div>
+    )
+}
+
+export default Profile;
