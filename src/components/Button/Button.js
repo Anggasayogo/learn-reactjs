9b96de887e7aaa67cb8a669505f67a26403d2346
anggasayogo <anggasayogosm@gmail.com>
2020-06-27 09:45:00 +0700

    update counter tambah data dan kurang data

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -4,13 +4,22 @@ import './Button.css';
 const Button = () => {
     const [angka,setAngka] = useState(0);
 
-    const handleTouch = () => {
+    const handleTambah = () => {
         setAngka(angka + 1);
+    }
+
+    const handleKurang = () =>{
+        setAngka(angka - 1)
     }
     return (
         <div>
             <h3>Count {angka} </h3>
-            <button id="btn" type="submit" onClick={handleTouch} />
+            <button id="btn" type="submit" onClick={handleTambah}>
+                Tambah Data
+            </button>
+            <button id="btn" type="submit" onClick={handleKurang}>
+                Kurang Data
+            </button>
         </div>
     )
 }
